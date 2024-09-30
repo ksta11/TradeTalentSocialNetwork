ff4f4ce8a92b4a77677ac34b2c8b21b845c44fe6
Santiago <santicasta1003@gmail.com>
2024-09-30 14:49:18 -0500

    Se configuro ´de nuevo el router

--- TTSN/src/main.jsx
+++ TTSN/src/main.jsx
@@ -3,13 +3,18 @@ import { createRoot } from 'react-dom/client'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import Home from './pages/Home.jsx'
+
+
+import { Home, Login, Account, Register } from './pages'
 
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import App from './App.jsx'
 
+
+
 const router = createBrowserRouter([
-  { path: "/", element: <Home />}
+  { path: "/", element: <Home />},
+  { path: "/account", element: <Account />}
 ])
 
 createRoot(document.getElementById('root')).render(
