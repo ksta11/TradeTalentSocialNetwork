a47cdae3f946a23580e6a8aa88c54a960a8e50a9
Santiago <santicasta1003@gmail.com>
2024-10-07 13:34:01 -0500

    Se modifico el funcionamiento y diseño del navbar

--- TTSN/src/components/molecules/Navbar.jsx
+++ TTSN/src/components/molecules/Navbar.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import HomeLink from '../atoms/HomeLink';
-import LoginLink from '../atoms/LoginLink';
-import RegisterLink from '../atoms/RegisterLink';
+import IconLink from '../atoms/IconLink';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div className='container-fluid'>
-      <div className='row'>
-        <div className='col-12'>
-          <Nav className="flex-column bg-light sidebar">
-            <HomeLink />
-            <LoginLink />
-            <RegisterLink />
+    <div className='container-fluid sidebar-container'>
+      <div className='row h-100'>
+        <div className='col-12 p-0'>
+          <Nav className="flex-column bg-light sidebar h-100 justify-content-center align-items-center">
+            <IconLink icon="bi-house-door" to="/" />
+            <IconLink icon="bi-person" to="/login" />
+            <IconLink icon="bi-person-plus" to="/register" />
             {/* Si necesitas un enlace de registro, crea un componente RegisterLink similar */}
           </Nav>
         </div>
