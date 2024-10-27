9f56ad72a836bee3245e13bc819360a3724982c4
JacoboAP1 <jacobo.arroyavep@autonoma.edu.co>
2024-10-27 01:14:49 -0500

    Se implementó una estética visual en css para login y se añadieron pequeños ajustes jsx para la división de los contenedores

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className='row h-100'>
         <div className='col-12 p-0'>
           <Nav className="flex-column sidebar h-100 justify-content-center align-items-center">
-            <IconLink icon="bi-house-door" to="/" tooltipText="SOy gay"/>
+            <IconLink icon="bi-house-door" to="/" tooltipText="Home"/>
             <IconLink icon="bi-person" to="/login" tooltipText="Iniciar sesión"/>
             <IconLink icon="bi-person-plus" to="/register" tooltipText="Registrarse"/>
           </Nav>
