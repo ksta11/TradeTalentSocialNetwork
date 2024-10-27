ae7bcf8a01180dc01411a0258f2f791cceb4bfe9
David Toro <davidesteban0736@.com>
2024-10-26 21:22:43 -0500

    se mejora el responsive de los registros

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -16,74 +16,62 @@ const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 
   return (
-
     <form onSubmit={handleSubmit(onSubmit)}> 
-
-      <div className="container-fluid">
-
-              <div className='row justify-content-center mb-3 mt-5'>
-                    <div className='col-2'>
-                        <h1>Iniciar Sesión</h1>
-                    </div>
-              </div>
-          
-
-              <div className='row justify-content-center mb-3'>
-                    <div className='col-4'>
-                      <FormTextField
-                        label="Correo electrónico"
-                        id="email"
-                        type="email"
-                        ariaDescribedby="emailHelpBlock"
-                        helpText=""
-                        {...register("email",{
-                          required:{
-                          value:true,
-                          message:"Correo es requerido"
-                      
-                          },
-                          pattern:{
-                              value:emailRegex,
-                              message:"Correo no válido"
-                          }
-
-                  })}
-                  />
-                  {errors.email && <span>{errors.email.message}</span>}
-                    </div>
-              </div>
-
-
-              <div className='row justify-content-center mb-3'>
-                    <div className='col-4'>
-                      <FormTextField
-                      label="Contraseña"
-                      id="password"
-                      type="password"
-                      ariaDescribedby="passwordHelpBlock"
-                      helpText=""
-                      {...register("password",{
-                        required:{
-                            value:true,
-                            message:"Debe de ingresar una contraseña"
-                        },
-                        minLength:{
-                            value:6,
-                            message:"Contraseña debe de tener mínimo 6 caracteres"
-                        }
-                    
-                    })}
-                />
-                {errors.password && <span>{errors.password.message}</span>}
-                    </div>
-              </div>
-          
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                  <Button_type label="Iniciar sesión" size="lg"/>
-              </div>
-
-
-
+      <div className="container mt-">
+        <div className='row justify-content-center mb-3 mt-5'>
+          <div className='col-12 col-sm-8 col-md-6 col-lg-4'>
+            <h1 className="text-center">Iniciar Sesión</h1>
+          </div>
+        </div>
+        <div className='row justify-content-center mb-3'>
+          <div className='col-12 col-sm-8 col-md-6 col-lg-4'>
+            <FormTextField
+              label="Correo electrónico"
+              id="email"
+              type="email"
+              ariaDescribedby="emailHelpBlock"
+              helpText=""
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Correo es requerido"
+                },
+                pattern: {
+                  value: emailRegex,
+                  message: "Correo no válido"
+                }
+              })}
+            />
+            {errors.email && <span>{errors.email.message}</span>}
+          </div>
+        </div>
+        <div className='row justify-content-center mb-3'>
+          <div className='col-12 col-sm-8 col-md-6 col-lg-4'>
+            <FormTextField
+              label="Contraseña"
+              id="password"
+              type="password"
+              ariaDescribedby="passwordHelpBlock"
+              helpText=""
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Debe de ingresar una contraseña"
+                },
+                minLength: {
+                  value: 6,
+                  message: "Contraseña debe de tener mínimo 6 caracteres"
+                }
+              })}
+            />
+            {errors.password && <span>{errors.password.message}</span>}
+          </div>
+        </div>
+        <div className='row justify-content-center'>
+          <div className='col-12 col-sm-8 col-md-6 col-lg-4 text-center'>
+            <Button_type label="Iniciar sesión" size="lg"/>
+          </div>
+        </div>
       </div>
     </form>
   );
